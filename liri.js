c906dca0db5a7f97b4a82190989b96f49ea30378
Gustavo Garcia <Gustavo.Garcia89@outlook.com>
2020-01-11 01:15:33 -0500

    update

--- liri.js
+++ liri.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-//Add the code required to import the keys.js file and store it in a variable.
-
 var keys = require("./keys");
 
 var spotify = new Spotify(keys.spotify);
